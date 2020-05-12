bf0500a23aa6d4c2dbe6bee36f66e7c31471f785
toku-sa-n <tokusan441@gmail.com>
2020-05-13 00:01:44 +0900

    [Add]Another bug
    rlibc is deprecated and should be replaced with some other library.

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	cargo build -Zbuild-std=core,alloc
+	ld -nostdlib -T os.ld target/cargo_settings/debug/libramen_os.a
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
 #![no_std]
 
+extern crate rlibc;
+
+const TWO_DIMENSIONAL_ARRAY: [[char; WIDTH]; HEIGHT] = [
+    ['1', '2', '3', '4', '5', '6'],
+    ['1', '2', '3', '4', '5', '6'],
+    ['1', '2', '3', '4', '5', '6'],
+    ['1', '2', '3', '4', '5', '6'],
+    ['1', '2', '3', '4', '5', '6'],
+    ['1', '2', '3', '4', '5', '6'],
+];
+
+const WIDTH: usize = 6;
+const HEIGHT: usize = 6;
+
+fn foo(_arr: [[char; WIDTH]; HEIGHT]) -> () {
+    loop {}
+}
+
+#[no_mangle]
+fn os_main() {
+    foo(TWO_DIMENSIONAL_ARRAY);
+}
+
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
